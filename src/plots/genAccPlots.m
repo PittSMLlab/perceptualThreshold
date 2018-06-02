3dbe0dd27a86f615b02d95ffdb53ffb1d93e081c
Unknown <itu.pablo@gmail.com>
2018-06-02 17:56:37 -0400

    More simulations

--- src/plots/genAccPlots.m
+++ src/plots/genAccPlots.m
@@ -1,51 +1,6 @@
 %% Generate accuracy plots for each subject
-addpath(genpath('./'));
-%% This file assumes the existence of csv files that summarize block results. If this is not the case, run processDatlogs.m
 %%
-baseDir='../data/AA0';
-figDir='../fig/AA0';
-    subID=[];
-for j=1:9 %All subjects
-    exit=false;
-    blockNo=0;
-    subDir=[baseDir num2str(j) '/'];
-    figDir1=[figDir num2str(j) '/'];
-   while ~exit 
-       blockNo=blockNo+1;
-       try
-          t=readtable([subDir 'Block' num2str(blockNo) '.csv']);
-          if false %blockNo==3 %To drop block 3 from subjects with odd number of blocks
-              try %Try loading block 4, if fails (no block 4), dont use block 3 either
-                  t4=readtable([subDir 'Block' num2str(blockNo+1) '.csv']);
-              catch
-                  exit=true;
-                  break
-              end
-          end
-       catch %Went trhough all blocks for this sub
-           exit=true;
-           break
-       end
-          %fh=accuracyPlots(t);
-          %saveFig(fh,subDir,['accBlock' num2str(blockNo)],0)
-          taux=table(j.*ones(size(t.date,1),1), blockNo*ones(size(t.date,1),1),'VariableNames',{'subID','blockNo'});
-          t=cat(2,t,taux);
-          if blockNo==1
-              superT=t;
-          else
-             superT=cat(1,superT,t);
-          end
-
-   end
-    %fh=accuracyPlots(superT);
-    %fh.Name=['Accuracy for subject AA0' num2str(j)];
-    %saveFig(fh,figDir1,['accuracyAA0' num2str(j)],0)
-    if j==1
-        superSuperT=superT;
-    else
-        superSuperT=cat(1,superSuperT,superT);
-    end
-end
+loadAllDataIntoTable
 %%
 fh=accPlots(superSuperT);
 saveFig(fh,'../fig/all/',['accuracyAll'],0)
--- src/testRandomWalkEndTimes.m
+++ src/testRandomWalkEndTimes.m
@@ -1,35 +1,72 @@
+%% load empirical data for comparison
+%loadAllDataIntoTable
 %% Simulate random walk threshold times
-N=1e4;
-s=.08;
+N=30; %Simulation steps
+s=.62;
 th=1;
-M=1e4;
-P=10;
+M=1e5;
+drifts=[0 10 25 50 75 100 125 150 200 250 300 350];
+P=length(drifts);
 correct=false(M,P);
 t=nan(M,P);
 for j=1:P
-    m=(j-1)/400;
+    m=drifts(j)/5e2+drifts(j)^2/5e4;
     for k=1:M %Number of sims
         x=zeros(N,1);
         for i=2:N
             x(i)=x(i-1)+m+s*randn;
             if abs(x(i))>th
+                t(k,j)=i;
+                correct(k,j)=x(i)>th;
                 break
             end
         end
-        t(k,j)=i;
-        correct(k,j)=x(i)>th;
     end
 end
 
+
+
 %%
 figure
-subplot(2,1,1)
-histogram(t,'Normalization','pdf')
+subplot(2,2,1)
+for j=1:P
+histogram(t(:,j),'Normalization','probability','EdgeColor','none','FaceAlpha',.2)
 hold on
-c=(th/s)^2;
-mu=0;
-x=[.01:.01:N];
-plot(x,1.8*sqrt(c/(2*pi))*exp(-c./(2*(x-mu)))./(x-mu).^(3/2))
+end
+title('Simulated')
+axis([0 15 0 .5])
+subplot(2,2,2)
+for j=1:P
+        dataT=superSuperT(superSuperT.pertSize==drifts(j) | superSuperT.pertSize==-drifts(j),:);
+        histogram(dataT.reactionStride,'Normalization','probability','EdgeColor','none','FaceAlpha',.2)
+        hold on
+        empAcc(j,:)=sign(dataT.initialResponse)==-sign(dataT.pertSize);
+        empMeanRS(j)=nanmean(dataT.reactionStride);
+        empStdRS(j)=nanstd(dataT.reactionStride);
+end
+title('Empirical')
+axis([0 15 0 .5])
+%c=(th/s)^2;
+%mu=0;
+%x=[.01:.01:N];
+%plot(x,1.8*sqrt(c/(2*pi))*exp(-c./(2*(x-mu)))./(x-mu).^(3/2))
 
-subplot(2,1,2)
-plot([0:P-1]/400,mean(correct))
+subplot(2,2,4)
+title('End time')
+plot(drifts,nanmean(t,1),'LineWidth',2,'DisplayName','Mean')
+hold on
+plot(drifts,empMeanRS,'o','MarkerSize',5,'LineWidth',2,'DisplayName','Empirical mean')
+%plot([0:P-1]/600,1e-3./([0:P-1]/600).^2,'LineWidth',1,'DisplayName','Mean (theory)')
+plot(drifts,nanstd(t,1),'LineWidth',2,'DisplayName','Std')
+plot(drifts,empStdRS,'o','MarkerSize',5,'LineWidth',2,'DisplayName','Empirical std')
+xlabel('Perturbation (mm/s)')
+ylabel('Reaction stride (s)')
+legend
+
+subplot(2,2,3)
+plot(drifts,mean(correct),'LineWidth',2,'DisplayName','Model')
+hold on
+plot(drifts,mean(empAcc,2),'o','MarkerSize',5,'LineWidth',2,'DisplayName','Model')
+grid on
+title('Accuracy')
+ylabel('Correct decisions')
