d63a31350900a09c70270170fdddfd603d2a7708
Unknown <itu.pablo@gmail.com>
2018-06-07 21:30:02 -0400

    A better model

--- src/modeling/EZdifussion/optimizingThesholdForDD.m
+++ src/modeling/EZdifussion/optimizingThesholdForDD.m
@@ -11,14 +11,17 @@ t=[1:N]/N;
 th=1;
 a=10;
 alpha=1;
-beta=10;
+beta=2;
 thresholdCurve=th*(1+a*t.^alpha).*(1-t.^beta);
-thresholdCurve=.3+.5./(t+.2);
-thresholdCurve=1.5*max(1.7,.4./(2*t+.2)).*(1-t.^beta);
-thresholdCurve=max(2*(1-t),1);
-thresholdCurve=2*ones(size(t));
-Nsim=1e3;
-drifts=[0 .5 1 2 3];
+thresholdCurve=.1./(t+.15);
+%thresholdCurve=1.5*max(1.7,.4./(2*t+.2)).*(1-t.^beta);
+%thresholdCurve=max(2*(1-t),1);
+thresholdCurve=1.5*(ones(size(t))-t.^3);
+beta=10;
+thresholdCurve=(min(1.7*sqrt(.001+N*t*.01),4)).*max((1-(1.1*t).^beta),0);
+%thresholdCurve=.5*ones(size(t));
+Nsim=3e3;
+drifts=[0 .1 .2 .5 1 2];
 P=length(drifts);
 [endTime,correctResponse]=simulateEZ(thresholdCurve,drifts,Nsim);
 correctRate=reshape(mean(correctResponse),P,1);
@@ -40,11 +43,11 @@ for l=1:P
     subplot(4,1,1)
     hold on
     histogram(endTime(correctResponse(:,l,1),l,1),[0:.33:30],'FaceColor',ppp.Color,'FaceAlpha',.4,'EdgeColor','none','Normalization','probability')
-    axis([0 30 0 .25])
+    axis([0 15 0 .35])
     subplot(4,1,3)
     hold on
     histogram(endTime(~correctResponse(:,l,1),l,1),[0:.33:30],'FaceColor',ppp.Color,'FaceAlpha',.5,'EdgeColor','none','Normalization','probability')
-    axis([0 30 0 .25])
+    axis([0 15 0 .35])
         subplot(4,4,13)
     hold on
     plot(mean(endTime(:,l,1)),mean(correctResponse(:,l,1)),'o','Color',ppp.Color)
