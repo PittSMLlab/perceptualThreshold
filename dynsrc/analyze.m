2adc08db95d5becdd080b99149799728aa460759
Unknown <itu.pablo@gmail.com>
2018-07-18 16:17:20 -0400

    Added AB05

--- dynsrc/analyze.m
+++ dynsrc/analyze.m
@@ -59,25 +59,50 @@ for j=1:length(subList)
 end
 %% Plot adaptation
 figure;
-subplot(3,1,1)
-%scatter(allTA.startTime(allTA.pertSize==200),200,50,[1,0,0],'filled')
-hold on
-scatter(allTA.startTime(allTA.pertSize==200),allTA.lastSpeedDiff(allTA.pertSize==200),50,[1,0,0])
-%scatter(allTA.startTime(allTA.pertSize==400),400,50,[0,0,1],'filled')
-scatter(allTA.startTime(allTA.pertSize==400),allTA.lastSpeedDiff(allTA.pertSize==400),50,[0,0,1])
-grid on
-title('Last Speed Diff')
+vars={'lastSpeedDiff','netClicks','reactionTime'};
+allTA.netClicks=allTA.Lclicks-allTA.Rclicks;
+allT.netClicks=allT.Lclicks-allT.Rclicks;
+for k=1:3
+    v=allTA.(vars{k});
+    vB=allT.(vars{k});
+
+    subplot(3,1,k)
+    hold on
+    aux=mod([1:length(allTA.startTime)]-1,19);
+    scatter(aux(allTA.pertSize==200),v(allTA.pertSize==200),50,[1,0,0])
+    scatter(aux(allTA.pertSize==400),v(allTA.pertSize==400),50,[0,0,1])
+    for i=0:18
+        scatter(i,mean(v(aux==i)),100,[-1,0,1]*(mean(allTA.pertSize(aux==i))/200-1) +[1,0,0],'filled')
+    end
+    scatter(-2,mean(vB(allT.pertSize==200)),100,[.7,.2,0],'filled')
+    scatter(-2*ones(sum(allT.pertSize==200),1),vB(allT.pertSize==200),50,[.7,.2,0])
+    axis tight
+    pp=patch([.8 17.8 17.8 .8],[min(v)*[1 1] max(v)*[1 1]],.6*ones(1,3),'FaceAlpha',.3,'EdgeColor','none');
+    uistack(pp,'bottom')
+    grid on
+    title(vars{k})
+end
+
+%%
 subplot(3,1,2)
 %scatter(allTA.startTime(allTA.pertSize==200),200,50,[1,0,0],'filled')
 hold on
-scatter(allTA.startTime(allTA.pertSize==200),allTA.Lclicks(allTA.pertSize==200)-allTA.Rclicks(allTA.pertSize==200),50,[1,0,0])
+scatter(aux(allTA.pertSize==200),allTA.Lclicks(allTA.pertSize==200)-allTA.Rclicks(allTA.pertSize==200),50,[1,0,0])
 %scatter(allTA.startTime(allTA.pertSize==400),400,50,[0,0,1],'filled')
-scatter(allTA.startTime(allTA.pertSize==400),allTA.Lclicks(allTA.pertSize==400)-allTA.Rclicks(allTA.pertSize==400),50,[0,0,1])
+scatter(aux(allTA.pertSize==400),allTA.Lclicks(allTA.pertSize==400)-allTA.Rclicks(allTA.pertSize==400),50,[0,0,1])
+for i=0:18
+    scatter(i,mean(allTA.Lclicks(aux==i)-allTA.Rclicks(aux==i)),100,[-1,0,1]*(mean(allTA.pertSize(aux==i))/200-1) +[1,0,0],'filled')
+end
 grid on
 title('Net clicks')
+
+%
 subplot(3,1,3)
 hold on
-scatter(allTA.startTime(allTA.pertSize==200),allTA.reactionTime(allTA.pertSize==200),50,[1,0,0])
-scatter(allTA.startTime(allTA.pertSize==400),allTA.reactionTime(allTA.pertSize==400),50,[0,0,1])
+scatter(aux(allTA.pertSize==200),allTA.reactionTime(allTA.pertSize==200),50,[1,0,0])
+scatter(aux(allTA.pertSize==400),allTA.reactionTime(allTA.pertSize==400),50,[0,0,1])
+for i=0:18
+    scatter(i,nanmean(allTA.reactionTime(aux==i)),100,[-1,0,1]*(mean(allTA.pertSize(aux==i))/200-1) +[1,0,0],'filled')
+end
 title('Reaction times')
 grid on
