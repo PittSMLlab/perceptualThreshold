848a6c73d0c655a1bdcca808e72962c34d37d1e9
Unknown <itu.pablo@gmail.com>
2018-07-26 21:25:52 -0400

    Added all subjects up to AB10 and improved analyze script

--- dynsrc/analyze.m
+++ dynsrc/analyze.m
@@ -57,52 +57,86 @@ for j=1:length(subList)
         allTA=td;
     end
 end
+%% Get post trials
+for j=1:length(subList)
+    td=readtable([dataDir subList(j).name '/PostAdaptation.csv']);
+    if j>1
+       allTP=[allTP;td];
+    else
+        allTP=td;
+    end
+end
+
 %% Plot adaptation
+func=@(x) nanmedian(x);
 figure;
 vars={'lastSpeedDiff','netClicks','reactionTime'};
 allTA.netClicks=allTA.Lclicks-allTA.Rclicks;
 allT.netClicks=allT.Lclicks-allT.Rclicks;
+allTP.netClicks=allTP.Lclicks-allTP.Rclicks;
+colors=get(gca,'ColorOrder');
 for k=1:3
     v=allTA.(vars{k});
     vB=allT.(vars{k});
+    vP=allTP.(vars{k});
 
     subplot(3,1,k)
     hold on
+    %Adapt:
     aux=mod([1:length(allTA.startTime)]-1,19);
-    scatter(aux(allTA.pertSize==200),v(allTA.pertSize==200),50,[1,0,0])
-    scatter(aux(allTA.pertSize==400),v(allTA.pertSize==400),50,[0,0,1])
-    for i=0:18
-        scatter(i,mean(v(aux==i)),100,[-1,0,1]*(mean(allTA.pertSize(aux==i))/200-1) +[1,0,0],'filled')
+    pp=unique(allTA.pertSize);
+    for j=1:length(pp)
+    scatter(aux(allTA.pertSize==pp(j)),v(allTA.pertSize==pp(j)),50,colors(pp(j)/100+3,:))
     end
-    scatter(-2,mean(vB(allT.pertSize==200)),100,[.7,.2,0],'filled')
-    scatter(-2*ones(sum(allT.pertSize==200),1),vB(allT.pertSize==200),50,[.7,.2,0])
+    %scatter(aux(allTA.pertSize==400),v(allTA.pertSize==400),50,[0,0,1])
+    for i=0:18
+        scatter(i,func(v(aux==i)),100,colors(allTA.pertSize(i+1)/100 + 3,:),'filled')
+    end
+    
+    %Base:
+    pp=unique(allT.pertSize);
+    for j=1:length(pp)
+        scatter(-10+j,func(vB(allT.pertSize==pp(j))),50,colors(pp(j)/100+3,:),'filled')
+        scatter((-10+j)*ones(sum(allT.pertSize==pp(j)),1),(vB(allT.pertSize==pp(j))),50,colors(pp(j)/100+3,:))
+    end
+    %scatter(-2,func(vB(allT.pertSize==200)),100,[.7,.2,0],'filled')
+    %scatter(-2*ones(sum(allT.pertSize==200),1),vB(allT.pertSize==200),50,[.7,.2,0])
+    
+    %Paraphernalia
     axis tight
     pp=patch([.8 17.8 17.8 .8],[min(v)*[1 1] max(v)*[1 1]],.6*ones(1,3),'FaceAlpha',.3,'EdgeColor','none');
     uistack(pp,'bottom')
     grid on
     title(vars{k})
+    
+    % Add post adaptation:
+    aux=mod([1:length(allTP.startTime)]-1,13)+19;
+    pp=unique(allTP.pertSize);
+    pp=pp(1:4);
+    for j=1:length(pp)
+        scatter(aux(allTP.pertSize==pp(j)),vP(allTP.pertSize==pp(j)),50,colors(pp(j)/100+3,:))
+    end
+    for i=19:13+19
+        scatter(i,func(vP(aux==i)),100,colors(allTP.pertSize(i-18)/100 + 3,:),'filled')
+    end
+    %for i=0:12
+    %    scatter(i,func(vP(aux==i)),100,colors(allTP.pertSize(i+1)/100 + 3,:),'filled')
+    %end
 end
 
-%%
-subplot(3,1,2)
-%scatter(allTA.startTime(allTA.pertSize==200),200,50,[1,0,0],'filled')
-hold on
-scatter(aux(allTA.pertSize==200),allTA.Lclicks(allTA.pertSize==200)-allTA.Rclicks(allTA.pertSize==200),50,[1,0,0])
-%scatter(allTA.startTime(allTA.pertSize==400),400,50,[0,0,1],'filled')
-scatter(aux(allTA.pertSize==400),allTA.Lclicks(allTA.pertSize==400)-allTA.Rclicks(allTA.pertSize==400),50,[0,0,1])
-for i=0:18
-    scatter(i,mean(allTA.Lclicks(aux==i)-allTA.Rclicks(aux==i)),100,[-1,0,1]*(mean(allTA.pertSize(aux==i))/200-1) +[1,0,0],'filled')
-end
-grid on
-title('Net clicks')
 
-%
-subplot(3,1,3)
+%% Plot all click rates vs. reaction times
+figure;
+subplot(2,1,1)
 hold on
-scatter(aux(allTA.pertSize==200),allTA.reactionTime(allTA.pertSize==200),50,[1,0,0])
-scatter(aux(allTA.pertSize==400),allTA.reactionTime(allTA.pertSize==400),50,[0,0,1])
-for i=0:18
-    scatter(i,nanmean(allTA.reactionTime(aux==i)),100,[-1,0,1]*(mean(allTA.pertSize(aux==i))/200-1) +[1,0,0],'filled')
+for ps=[-200:100:200]
+    aux=allT.pertSize==ps;
+s=scatter((allT.reactionTime(aux)),(abs(allT.Lclicks(aux)-allT.Rclicks(aux))),20,'filled');
+scatter(nanmean((allT.reactionTime(aux))),mean(abs(allT.Lclicks(aux)-allT.Rclicks(aux))),100,s.CData,'filled');
 end
-title('Reaction times')
-grid on
+
+for ps=[200,400]
+    aux=allTA.pertSize==ps;
+s=scatter((allTA.reactionTime(aux)),(abs(allTA.Lclicks(aux)-allTA.Rclicks(aux))),20);
+scatter(nanmean((allTA.reactionTime(aux))),mean(abs(allTA.Lclicks(aux)-allTA.Rclicks(aux))),100,s.CData);
+end
