1f2c4bae3b52fa80ddc93e605f9209b96592e959
Pablo <piturralde@ieee.org>
2019-07-03 09:00:46 -0400

    Fully working trackPSE

--- src/trackPSE.m
+++ src/trackPSE.m
@@ -22,6 +22,8 @@ for j=sL
         end
 
      bs=readtable([dataDir subList(j).name '/BaselineSlow.csv']);  %Long-response time task (24 strides)
+             bs.blockNo=9*ones(size(bs,1),1);
+        bs.subID=j*ones(size(bs,1),1);
     if j>1
         allT1=[allT1;bs];
     else
@@ -76,32 +78,62 @@ pps=[pS; pS; pSa; pSp];
 
 v=[vRb-vLb; nan(breakSize,1); vRb-vLb; nan(breakSize,1); vR-vL; nan(breakSize,1); vRp-vLp]; %This will be the input
 
+%% Some pre processing
+allData=[allT;allTA;allTP];
+startTime=allData.date*1e5+allData.startTime;
+[~,idx]=sort(startTime,'ascend');
+allData=allData(idx,:);
+baseData=[allT;allT1];
+baseData.leftResponse=baseData.initialResponse==-1;
+trueResponses=baseData(~isnan(baseData.reactionTime),:);
+psychom=fitglm(trueResponses,'leftResponse~pertSize','Distribution','binomial');
+sigma=1/psychom.Coefficients.Estimate(2) %Empirically estimated sigma for accuracy response
+bias=-psychom.Coefficients.Estimate(1)/psychom.Coefficients.Estimate(2)
+baseData=[allT];
+baseData.leftResponse=baseData.initialResponse==-1;
+trueResponses=baseData(~isnan(baseData.reactionTime),:);
+rsp=fitglm(trueResponses,'lastSpeedDiff~pertSize')
+err=trueResponses.lastSpeedDiff-(rsp.Coefficients.Estimate(1)+rsp.Coefficients.Estimate(2)*trueResponses.pertSize);
+sig=std(err)
+bis=rsp.Coefficients.Estimate(2)
+
+
 %% Laundry list:
 %From baseline data: drop non-responses(?)
 %From remaining trials, estimate a distribution p(left| \Delta V)
 %We'll assume that p(left|\DeltaV) really is p(left|\Delta V - PSE), only
 input=[v];
 obsTimes=find(isnan(input(2:end)) & ~isnan(input(1:end-1)));
-allData=[allT;allTA;allTP];
-startTime=allData.date*1e5+allData.startTime;
-[~,idx]=sort(startTime,'ascend');
-allData=allData(idx,:);
-obs=discretizeObs(allData.initialResponse==1,2,[0,1]);
-obs2=discretizeObs(allData.lastSpeedDiff-allData.pertSize,50,[-300,300]);
+range=[-500:10:500];
+%range=[-300:6:300];
+dataUsed=1;
+switch dataUsed
+    case 1
+        obs=discretizeObs(allData.initialResponse==1,2,[0,1]);
+        %bias=0;
+        %sigma=75/1.1; %Realistic based on group-level analysis of responses. These numbers are from the static task. Do they match the dynamic one?
+        p=1./(1+exp((range+bias)/sigma));
+        pObsGivenState=[p;1-p];
+        O=@(u) [0;1]+[1;-1].*1./(1+exp((range+bias-u)/sigma));
+        oRange=[0 1];
+    case 2
+        obs=discretizeObs(allData.lastSpeedDiff,length(range),[min(range) max(range)]); %Same range
+        sig=50; %the RMSE from the static study fits
+        O=@(u) exp(-(range'-range).^2/(2*sig^2)); %Presuming that lastSpeedDiff is an unbiased estimator of PSE
+        O=@(u) exp(-(range'-range-bis*(u-range)).^2/(2*sig^2)); % 35% undercorrection, derived from base 
+        oRange=range;
+end
 obsTimes=repmat(obsTimes,10,1);
 
+%Remove nan responses:
+obs=obs(~isnan(allData.reactionTime));
+obsTimes=obsTimes(~isnan(allData.reactionTime));
+
+
 N=length(input);
-bias=0;
-sigma=75/1.1; %Realistic based on group-level analysis of responses
-range=[-500:10:500];
-p=1./(1+exp((range+bias)/sigma));
-pObsGivenState=[p;1-p];
-O=@(u) [0;1]+[1;-1].*1./(1+exp((range+bias-u)/sigma));
-O2=[]; %Obs matrix for adjustment task: linear correction with saturation, and variability.
-%that in baseline PSE=0
 %We'll assume some arbitrary transition matrices p(x_{k+1}|x_{k},u_{k}), which
-s=4;
-T=exp(-(range'-range).^2./(2*s^2));
+s=10;
+T=@(u) exp(-(range'-range).^2./(2*s^2));
 pStateInitial=ones(size(range'))/numel(range);
 N=length(input);
 %Run a hidden-markov model inference to get p(x_k| obs)
@@ -109,16 +141,35 @@ N=length(input);
 [pPredicted, pUpdated, pSmoothed] = HMMnonStationaryInferenceAlt(obs,obsTimes,input,O,T,pStateInitial);
 
 %Compute viterbi sequence:
-%[optSeq,logL]=nonStatViterbi(observations,pStateGivenPrev,pObsGivenState,pStateInitial,input,observationTimes);
+%[optSeq,logL]=nonStatViterbi(obs,T,O,pStateInitial,input,obsTimes);
 %Viz:
-[fh] = vizHMMInference(pSmoothed,T,O(0),obs,obsTimes,range,[0 1],1:N);
+[fh] = vizHMMInference(pSmoothed,T(0),O(0),obs,obsTimes,range,oRange,1:N);
+%[fh] = vizHMMInference(pUpdated,T(0),O(0),obs,obsTimes,range,oRange,1:N);
 
 %Add adapt/post separation\
 ph=findobj(fh,'Type','Axes');
 axes(ph(2))
 hold on
 plot(888*[1 1],500*[1 -1],'k')
+%plot(858*[1 1],500*[1 -1],'Color',.5*ones(1,3)) %Last measurement of base
+%plot(903*[1 1],500*[1 -1],'Color',.5*ones(1,3)) %First of A
 plot(1793*[1 1],500*[1 -1],'k')
+%plot(1763*[1 1],500*[1 -1],'Color',.5*ones(1,3)) %Last measurement of base
+%plot(1808*[1 1],500*[1 -1],'Color',.5*ones(1,3)) %First of A
+[~,ml]=max(pSmoothed);
+plot(1:N,range(ml),'k') %Ml states
+set(gca,'XTick',sort(unique(obsTimes)),'XTickLabel',{},'YTick',[-450:150:450])
+grid on
+if dataUsed==2 %Add viterbi sequence
+%plot(1:N,range(optSeq),'r')
+end
+if dataUsed==1
+    axes(ph(1))
+    fg=findgroups(obsTimes);
+   aux=splitapply(@(x) nanmean(x),obs,fg);
+   aux2=splitapply(@(x) nanmean(x),obsTimes,fg);
+   scatter(aux2,aux,'filled')
+end
 %% The real deal:
 %Get table with responses, generate observation vectors
 
